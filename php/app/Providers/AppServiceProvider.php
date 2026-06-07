3251f27a82d5a7acd846a1e6e5a7fe3dcaca9278
Jo2008 <magombejoseph2@gmail.com>
2026-06-07 23:18:14 +0200

    fix: mover Gate::policy para AppServiceProvider

--- php/app/Providers/AppServiceProvider.php
+++ php/app/Providers/AppServiceProvider.php
@@ -2,17 +2,26 @@
 
 namespace App\Providers;
 
+use App\Models\Categoria;
+use App\Models\Encomenda;
+use App\Models\Produto;
+use App\Models\Vendedor;
+use App\Policies\CategoriaPolicy;
+use App\Policies\EncomendaPolicy;
+use App\Policies\ProdutoPolicy;
+use App\Policies\VendedorPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void {}
 
     public function boot(): void
     {
-        //
+        Gate::policy(Produto::class,   ProdutoPolicy::class);
+        Gate::policy(Encomenda::class, EncomendaPolicy::class);
+        Gate::policy(Categoria::class, CategoriaPolicy::class);
+        Gate::policy(Vendedor::class,  VendedorPolicy::class);
     }
 }
